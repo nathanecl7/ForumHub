31228fbe3b94d5b6413a02f7326c02d64c0d0926
Nathane Lima <156957406+nathanecl7@users.noreply.github.com>
2024-07-03 12:23:08 -0300

    Add files via upload

---
+++ topicoController.java
@@ -0,0 +1,14 @@
+package ForumHub.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("topico")
+public class topicoController {
+    @PostMapping
+    public  void  topico(){
+
+    }
+}
